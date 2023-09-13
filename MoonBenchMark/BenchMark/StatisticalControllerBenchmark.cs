6008768bd78dcdfb0f73208f7719dbaf2085671e
Giang <nguyenvietgiang1110@gmail.com>
2023-09-13 18:44:56 +0700

    Benchmark(40%)

---
+++ MoonBenchMark/BenchMark/StatisticalControllerBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using MoonBussiness.Repository;
+using MoonDataAccess;
+using MoonFood.Controllers.v1;
+
+namespace MoonBenchMark.Benchmark
+{
+    [MemoryDiagnoser]
+    public class StatisticalControllerBenchmark
+    {
+        private readonly StatisticalController _controller;
+        private readonly StatisticalRepository _repository;
+
+        public StatisticalControllerBenchmark()
+        {
+            var dbContext = new DataContext();
+            _repository = new StatisticalRepository(dbContext);
+            _controller = new StatisticalController(_repository);
+        }
+
+        [Benchmark]
+        public void GetAccountStatisticsBenchmark()
+        {
+            // Thực hiện benchmark cho GetAccountStatistics
+            _controller.GetAccountStatistics(DateTime.Now);
+        }
+
+        [Benchmark]
+        public void GetOdersStatisticsBenchmark()
+        {
+            // Thực hiện benchmark cho GetOdersStatistics
+            _controller.GetOdersStatistics(DateTime.Now);
+        }
+    }
+}
---
+++ MoonBenchMark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using MoonBenchMark.Benchmark;
+
+BenchmarkRunner.Run<StatisticalControllerBenchmark>();
